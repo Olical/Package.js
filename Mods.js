525570bb0a7e0d8b3449d9182763059c489fe96d
Oliver Caldwell <oliver@flowdev.co.uk>
2011-10-06 14:28:06 +0100

    Added the main header comment

--- Mods.js
+++ Mods.js
@@ -1,4 +1,9 @@
 /**
+ * @preserve Mods.js - Add class and package support to the browser
+ * Copyright 2011, Oliver Caldwell (olivercaldwell.co.uk)
+ **/
+
+/**
  * Class - Can extend, implement and require other classes
  **/
 function Class(settings) {
