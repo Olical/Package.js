81d1798c8d0525448f47d86bedc2490d112e2995
Oliver Caldwell <oliver@flowdev.co.uk>
2011-10-07 08:35:16 +0100

    Added an initialisation test.

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,18 @@
+describe('Class', function () {
+	it('can be initialised', function() {
+		var TestClass = new Class({
+			Constructor: function() {
+				this.toCheck = true;
+			}
+		});
+		
+		var test = new TestClass();
+		
+		expect(test.toCheck).toEqual(true);
+	});
+});
+
+// Display HTML output
 var jasmineEnv = jasmine.getEnv();
 jasmineEnv.reporter = new jasmine.TrivialReporter();
 jasmineEnv.execute();
