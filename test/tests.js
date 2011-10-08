41a1543ce5eb1e91cffd2ecc66652028a448353e
Oliver Caldwell <oliver@flowdev.co.uk>
2011-10-08 17:16:27 +0100

    Added implementation tests.

--- test/tests.js
+++ test/tests.js
@@ -55,6 +55,56 @@ describe('Class', function () {
 		expect(test.name).toEqual('Gerty');
 		expect(test.bark()).toEqual('Gerty barked! It was very quiet.');
 	});
+	
+	it('can implement another class', function() {
+		var Persistance = new Structure.Class({
+			save: function(key, value) {
+				localStorage.setItem(key, JSON.stringify(value));
+			},
+			load: function(key) {
+				return JSON.parse(localStorage.getItem(key));
+			},
+			remove: function(key) {
+				localStorage.removeItem(key);
+			}
+		});
+		
+		var PersistantObject = new Structure.Class({
+			Implements: Persistance,
+			Constructor: function(name) {
+				this.name = name;
+			},
+			data: {},
+			set: function(key, value) {
+				this.data[key] = value;
+			},
+			get: function(key) {
+				return this.data[key];
+			},
+			save: function() {
+				this.parent(this.name, this.data);
+			},
+			load: function() {
+				this.parent(this.name);
+			},
+			remove: function() {
+				this.parent(this.name);
+			}
+		});
+		
+		var test = new PersistantObject('test');
+		test.set('foo', 'bar');
+		test.save();
+		
+		expect(test.get('foo')).toEqual('bar');
+		
+		var test2 = new PersistantObject('test');
+		test2.load();
+		
+		expect(test2.get('foo')).toEqual('bar');
+		
+		test2.remove();
+	});
 });
 
 // Display HTML output
