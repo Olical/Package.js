df46d9a89638f02c1d6e97843982b2938f7e8a8d
Oliver Caldwell <oliver@flowdev.co.uk>
2011-10-10 21:13:48 +0100

    Added the implementMethods function. Need to add the wrap function dependency though.

--- Structure.js
+++ Structure.js
@@ -10,6 +10,34 @@
 	 * @param {Object} options The settings to initialise the class with
 	 **/
 	function Class(options) {
+		/**
+		 * Implements methods into an object
+		 *
+		 * @param {Object} methods The methods to implement
+		 * @param {Object} target The object to implement the methods into
+		 **/
+		function implementMethods(methods, target) {
+			// Initialise variables
+			var key = null;
+			
+			// Loop over the methods
+			for(key in methods) {
+				// Make sure it is not part of the prototype
+				if(methods.hasOwnProperty(key)) {
+					// If it is a function
+					// And it already exists
+					// Wrap it
+					if(typeof methods[key] === 'function' && target[key]) {
+						
+					}
+					else {
+						// Otherwise implement it
+						target[key] = methods[key];
+					}
+				}
+			}
+		}
+		
 		// Initialise the new class
 		function built() {
 			// If there is a constructor, call it
