843e2b10c991df8d3d65e575d92be1e15a3d5114
Oliver Caldwell <oliver@flowdev.co.uk>
2011-10-14 14:27:52 +0100

    Swapped to a method version of storing the path and added the load method.

--- Structure.js
+++ Structure.js
@@ -11,10 +11,36 @@
 	 *
 	 * @param {String} path The packages path, for example `graphics.canvas.Image`
 	 **/
-	function Package(path) {
-		// Store the path
-		this.path = path;
+	function Package(path, callback) {
+		// Check for a passed path
+		if(path) {
+			// Store the path
+			this.storePath(path);
+			
+			// If there is also a callback, load the package
+			if(callback) {
+				this.load();
+			}
+		}
 	}
+	
+	/**
+	 * Stores the class path
+	 *
+	 * @param {String} path The packages path, for example `graphics.canvas.Image`
+	 **/
+	Package.prototype.setPath = function(path) {
+		this.path = path;
+	};
+	
+	/**
+	 * Loads the current package and calls the passed callback when done
+	 *
+	 * @param {Function} callback Function to be run on completion
+	 **/
+	Package.prototype.load = function(callback) {
+		
+	};
 	
 	// Expose the variables
 	exports.Package = Package;
