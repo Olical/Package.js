7fc4e8ce6478aea674632f9953d2da8046f55bc4
Oliver Caldwell <oliver@flowdev.co.uk>
2011-10-14 14:17:30 +0100

    Removed all class code and comments. Going to pure packages.

--- Structure.js
+++ Structure.js
@@ -1,38 +1,10 @@
 /**
- * @preserve Structure.js - Add class and package support to the browser
+ * @preserve Structure.js - Add package support to the browser
  * Copyright 2011, Oliver Caldwell (olivercaldwell.co.uk)
  *
  * Licenced under GPL v3 <http://www.gnu.org/licences/gpl.html>
  **/
 
 (function(exports) {
-	/**
-	 * Used to create classes
-	 **/
-	function ClassFactory(options) {
-		// If options where passed implement them
-		if(options) {
-			this.options = options;
-			this.implement(this.options);
-		}
-	}
 	
-	/**
-	 * 
-	 **/
-	ClassFactory.prototype.implement = function(options) {
-		
-	};
-	
-	/**
-	 * Class - Can extend, implement and require other classes
-	 *
-	 * @param {Object} options The settings to initialise the class with
-	 **/
-	function Class(options) {
-		
-	}
-	
-	// Expose the variables
-	exports.Class = Class;
 }(window || exports));
