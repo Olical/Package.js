4a4763b1bc1b5aa395c4251e3eb9defbcf7f7e84
Oliver Caldwell <oliver@flowdev.co.uk>
2011-10-15 10:54:26 +0100

    Added some basic initialisation tests.

--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,17 @@
-describe('Defining', function () {
+describe('Initialisation', function() {
 	it('can be initialised', function() {
-		
+		var test = new Package();
+	});
+	
+	it('can be initialised with a path to set', function() {
+		var test = new Package('tests.MainTest');
+	});
+	
+	it('can be initialised with an object to set', function() {
+		var test = new Package({
+			path: 'tests.MainTest',
+			root: '/assets/javascript/packages'
+		});
 	});
 });
 
